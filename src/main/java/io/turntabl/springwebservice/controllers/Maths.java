8fd3ed65364defc6be7fcfba24e4fb5588532310
idawud <ismaildawud96@gmail.com>
2019-11-20 14:06:44 +0000

    test..

--- src/main/java/io/turntabl/springwebservice/controllers/Maths.java
+++ src/main/java/io/turntabl/springwebservice/controllers/Maths.java
@@ -1,8 +1,5 @@
 package io.turntabl.springwebservice.controllers;
 
-import org.springframework.context.annotation.Bean;
-
-
 public class Maths {
     public int add(int num1, int num2){
         return num1 + num2;
--- src/test/java/io/turntabl/springwebservice/controllers/MathsTest.java
+++ src/test/java/io/turntabl/springwebservice/controllers/MathsTest.java
@@ -1,16 +1,27 @@
 package io.turntabl.springwebservice.controllers;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MathsTest {
+    private Maths maths;
+
+    @Before
+    public void setup(){
+        maths = new Maths();
+    }
 
     @Test
-    public void add() {
+    public void add_Zero() {
+        int result = maths.add(0, 0);
+        assertEquals(0, result);
     }
 
     @Test
     public void subtract() {
+        int result = maths.subtract(0, 0);
+        assertEquals(0, result);
     }
 }
