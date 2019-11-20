20afcb61486f4c90465fd4fb338f5606e6bc4264
idawud <ismaildawud96@gmail.com>
2019-11-20 12:27:22 +0000

    maths beans

--- src/main/java/io/turntabl/springwebservice/controllers/MathsController.java
+++ src/main/java/io/turntabl/springwebservice/controllers/MathsController.java
@@ -1,12 +1,15 @@
 package io.turntabl.springwebservice.controllers;
 
 import io.turntabl.springwebservice.models.Number;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathsController {
+    @Autowired
+    private Maths operation;
 
     @GetMapping("add")
     public Number add(
@@ -16,9 +19,9 @@ public class MathsController {
             Integer num2){
         int first = Integer.parseInt(String.valueOf(num1));
         int second = Integer.parseInt(String.valueOf(num2));
-        int value = first + second;
+
         Number number = new Number();
-        number.setValue(value);
+        number.setValue(this.operation.add(first, second));
         return number;
     }
 
@@ -30,9 +33,9 @@ public class MathsController {
                     Integer num2){
         int first = Integer.parseInt(String.valueOf(num1));
         int second = Integer.parseInt(String.valueOf(num2));
-        int value = first - second;
+
         Number number = new Number();
-        number.setValue(value);
+        number.setValue(this.operation.subtract(first, second));
         return number;
     }
 
