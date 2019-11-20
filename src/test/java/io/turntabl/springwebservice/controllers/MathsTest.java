973693f3c1df727fc7d2a7b61432b2d7384457b5
idawud <ismaildawud96@gmail.com>
2019-11-20 14:25:19 +0000

    test..

--- src/test/java/io/turntabl/springwebservice/controllers/MathsTest.java
+++ src/test/java/io/turntabl/springwebservice/controllers/MathsTest.java
@@ -21,25 +21,42 @@ public class MathsTest {
 
     @Test
     public void add_Negatives() {
-        int result = maths.add(0, 0);
-        assertEquals(0, result);
+        int result = maths.add(-1, -1);
+        assertEquals(-2, result);
     }
 
     @Test
     public void add_positives() {
-        int result = maths.add(0, 0);
-        assertEquals(0, result);
+        int result = maths.add(1, 2);
+        assertEquals(3, result);
     }
     @Test
     public void add_pos_and_negative() {
-        int result = maths.add(0, 0);
+        int result = maths.add(1, -1);
         assertEquals(0, result);
     }
 
-
     @Test
-    public void subtract() {
+    public void subtract_zero() {
         int result = maths.subtract(0, 0);
         assertEquals(0, result);
+    }
+
+    @Test
+    public void subtract_positives() {
+        int result = maths.subtract(3, 2);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void subtract_negatives() {
+        int result = maths.subtract(-2, -1);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void subtract_pos_and_neg() {
+        int result = maths.subtract(3, -1);
+        assertEquals(4, result);
     }
 }
