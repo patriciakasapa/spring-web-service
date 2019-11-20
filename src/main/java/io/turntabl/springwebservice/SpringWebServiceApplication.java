835e39cdcb69360a32beebcc297b5184b53a1e35
idawud <ismaildawud96@gmail.com>
2019-11-20 15:46:30 +0000

    swagger docs

--- src/main/java/io/turntabl/springwebservice/SpringWebServiceApplication.java
+++ src/main/java/io/turntabl/springwebservice/SpringWebServiceApplication.java
@@ -4,7 +4,9 @@ import io.turntabl.springwebservice.controllers.Maths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SpringWebServiceApplication {
 
--- src/main/java/io/turntabl/springwebservice/controllers/MathsController.java
+++ src/main/java/io/turntabl/springwebservice/controllers/MathsController.java
@@ -1,19 +1,23 @@
 package io.turntabl.springwebservice.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.turntabl.springwebservice.models.Number;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class MathsController {
     @Autowired
     private Maths operation;
 
+    @ApiOperation("Adds two integer values ")
     @GetMapping("add")
     public Number add(
-            @RequestParam(name = "num1", defaultValue = "0")
+            @RequestParam(name = "num  1", defaultValue = "0")
             Integer num1,
             @RequestParam(name = "num2", defaultValue = "0")
             Integer num2){
@@ -25,6 +29,8 @@ public class MathsController {
         return number;
     }
 
+
+    @ApiOperation("Subtracts two integer values ")
     @GetMapping("subtract")
     public Number subtract(
             @RequestParam(name = "num1", defaultValue = "0")
