e608f085fb18b185a38153f2841cc88f778953a9
idawud <ismaildawud96@gmail.com>
2019-11-20 11:25:34 +0000

    add end

---
+++ src/main/java/io/turntabl/springwebservice/controllers/MathsController.java
@@ -0,0 +1,25 @@
+package io.turntabl.springwebservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathsController {
+
+    @GetMapping
+    public Number add(
+            @RequestParam(name = "num1", defaultValue = "0")
+            Integer num1,
+            @RequestParam(name = "num2", defaultValue = "0")
+            Integer num2){
+        int first = Integer.parseInt(String.valueOf(num1));
+        int second = Integer.parseInt(String.valueOf(num2));
+        return first + second;
+    }
+
+    @GetMapping
+    public Number subtract(Integer num1, Integer num2){
+        return 0;
+    }
+}
